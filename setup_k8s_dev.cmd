d6053c27e052dcf7217d71bc350f2e5a1615ec28
Dawid Lech Socha <xDayfit@gmail.com>
2025-08-02 20:56:24 +0200

    Add RabbitMQ configuration and application setup for Encryptify services

--- setup_k8s_dev.cmd
+++ setup_k8s_dev.cmd
@@ -13,16 +13,15 @@ helm upgrade --install data helm/Data ^
     --set global.postgresql.auth.username=%DB_USERNAME% ^
     --set global.postgresql.auth.password=%DB_PASSWORD% ^
     --set global.postgresql.auth.postgresPassword=%DB_SU_PASSWORD% ^
-    --set global.postgresql.auth.database=%DB_NAME%
+    --set global.postgresql.auth.database=%DB_NAME% ^
+    --set redis.auth.password=%REDIS_PASSWORD%
 
-helm upgrade --install rabbitmq helm/Communication ^
+helm upgrade --install communication helm/Communication ^
     --namespace communication ^
     --create-namespace ^
     --set rabbitmq.auth.username=%RABBITMQ_USER% ^
     --set rabbitmq.auth.password=%RABBITMQ_PASSWORD% ^
     --set rabbitmq.auth.su_username=%RABBITMQ_SU_USER% ^
-    --set rabbitmq.auth.su_password=%RABBIT_SU_PASSWORD%
+    --set rabbitmq.auth.su_password=%RABBITMQ_SU_PASSWORD%
 
-cmd /c netstat -ano | findstr /R ":5432[^0-9]" >nul && echo Port 5432 (PostgreSQL is probably running, skipping) || start "Port Forward" cmd /k kubectl port-forward svc/data-postgresql -n data 5432:5432
-cmd /c netstat -ano | findstr /R ":5672[^0-9]" >nul && echo Port 5672 (RabbitMQ is probably running, skipping) || start "Port Forward" cmd /k kubectl port-forward svc/rabbitmq -n communication 5672:5672
-cmd /c netstat -ano | findstr /R ":15672[^0-9]" >nul && echo Port 15672 (RabbitMQ UI is probably running, skipping) || start "Port Forward" cmd /k kubectl port-forward svc/rabbitmq -n communication 15672:15672
+echo To make cluster services work with local environment use 'telepresence connect --context=minikube' (if using minikube) command
